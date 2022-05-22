2e8eb8d448146f8213250dc561d80db1e761b9b9
Manuel <manuelalejandromartinezf@gmail.com>
2022-05-22 10:06:08 -0600

    Type classes

--- LEAN/type_classes.lean
+++ LEAN/type_classes.lean
@@ -232,6 +232,68 @@ end mtci
 
 namespace coer 
 
+  instance : Coe Bool Prop where 
+    coe b := b = true 
+
+  #eval if true then 5 else 3 -- 5
+
+  def Set (α : Type u) := α → Prop 
+  def Set.empty {α : Type u} : Set α := fun _ => False 
+  def Set.mem (a : α) (s : Set α) : Prop := s a 
+  def Set.singleton (a : α) : Set α := fun x => x = a 
+  def Set.union (a b : Set α) : Set α := fun x => a x ∨ b x 
+  notation "{" a "}" => Set.singleton a 
+  infix:55 " ∪ " => Set.union
+
+
+  def List.toSet : List α → Set α
+    | []    => Set.empty
+    | a::as => {a} ∪ toSet as 
+
+  instance : Coe (List α) (Set α) where 
+    coe as := List.toSet as 
+
+  /-
+  {1} ∪ List.toSet [1, 2] : Set Nat
+  -/
+  #check {1} ∪ ↑[1, 2] 
+
+
+  instance (p : Prop) [Decidable p] : CoeDep Prop p Bool where 
+    coe := decide p 
+
   
+  structure Semigroup where 
+    carrier : Type u
+    mul : carrier → carrier → carrier 
+    mul_assoc (a b c : carrier) : mul (mul a b) c = mul a (mul b c)
+
+  instance (S : Semigroup) : Mul S.carrier where 
+    mul := S.mul 
+
+  instance : CoeSort Semigroup (Type u) where 
+    coe s := s.carrier 
+
+  example (S : Semigroup) (a b c : S) : (a * b) * c = a * (b * c) := 
+    S.mul_assoc a b c 
+
+
+  structure Morphism (S1 S2 : Semigroup) where 
+    mor : S1 → S2 
+    resp_mul : ∀ a b : S1, mor (a * b) = mor a * mor b 
+  
+  #check @Morphism.mor
+
+  instance (S1 S2 : Semigroup) : CoeFun (Morphism S1 S2) (fun _ => S1 → S2) where 
+    coe m := m.mor 
+
+  theorem resp_mul {S1 S2 : Semigroup} (f : Morphism S1 S2) (a b : S1)
+      : f (a * b) = f a * f b := f.resp_mul a b 
+
+  example {S1 S2 : Semigroup} (f : Morphism S1 S2) (a : S1)
+      : f (a * a * a) = f a * f a * f a := by simp only [resp_mul]
+
+      
+
 
 end coer
